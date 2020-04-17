6b64c0a0f8c250d9d984acd494bdfeaf7141195f
Rami Sabbagh <ramilego4game@gmail.com>
2020-04-17 12:25:26 +0300

    Write the initialize of Update object
    Some fields are still missing

--- telegram/structures/Update.lua
+++ telegram/structures/Update.lua
@@ -20,6 +20,35 @@ local Update = class("telegram.structures.Update")
 -- @treturn MessageEntity The new created update data object.
 function Update:initialize(data)
 
+    --- The update‘s unique identifier (number).
+    -- Update identifiers start from a certain positive number and increase sequentially.
+    -- This ID becomes especially handy if you’re using Webhooks,
+    -- since it allows you to ignore repeated updates or to restore the correct update sequence,
+    -- should they get out of order.
+    -- If there are no new updates for at least a week,
+    -- then identifier of the next update will be chosen randomly instead of sequentially.
+    self.updateID = data.update_id
+
+    --- Optional fields.
+    -- @section optional_fields
+
+    --- New incoming message of any kind — text, photo, sticker, etc (Message).
+    self.message = data.message and Message(data.message)
+
+    --- New version of a message that is known to the bot and was edited (Message).
+    self.editedMessage = data.edited_message and Message(data.edited_message)
+
+    --- New incoming channel post of any kind — text, photo, sticker, etc (Message).
+    self.channelPost = data.channel_post and Message(data.channel_post)
+
+    --- New version of a channel post that is known to the bot and was edited (Message).
+    self.editedChannelPost = data.edited_channel_post and Message(data.edited_channel_post)
+
+    --TODO: inline_query, chosen_inline_result, callback_query, shipping_query,
+    --pre_checkout_query, poll, poll_answer
+
+    ---
+    -- @section end
 end
 
 return Update
