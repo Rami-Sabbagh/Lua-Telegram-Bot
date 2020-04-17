c0922266e7d5911b9641eef098ce7b35643fc2ba
Rami Sabbagh <ramilego4game@gmail.com>
2020-04-17 12:25:09 +0300

    Write the initialize of the Message object
    Some fields are still missing

--- telegram/structures/Message.lua
+++ telegram/structures/Message.lua
@@ -28,6 +28,185 @@ local Voice = require("telegram.structures.Voice")
 
 local Message = require("telegram.structures.Message")
 
---TODO: Message object.
+--- Create a new message object using data returned by Telegram Bot API.
+-- @tparam table data The message data returned by Telegram Bot API.
+-- @treturn Chat The new created message object.
+function Message:initialize(data)
+
+    --- Unique message identifier inside this chat (number).
+    self.messageID = data.message_id
+
+    --- Date the message was sent in Unix time (number).
+    self.date = data.date
+
+    --- Conversation the message belongs to (Chat).
+    self.chat = Chat(data.chat)
+
+    --- Optional fields.
+    -- @section optional_fields
+
+    --- Sender, empty for messages sent to channels (User).
+    self.from = data.from and User(data.from)
+
+    --- For forwarded messages, sender of the original message (User).
+    self.forwardFrom = data.forward_from and User(data.forward_from)
+
+    --- For messages forwarded from channels, information about the original channel (Chat).
+    self.forwardFromChat = data.forward_from_chat = User(data.forward_from_chat)
+
+    --- For messages forwarded from channels, identifier of the original message in the channel (number).
+    self.forwardFromMessageID = data.forward_from_message_id
+
+    --- For messages forwarded from channels, signature of the post author if present (string).
+    self.forwardSignature = data.forward_signature
+
+    --- Sender's name for messages forwarded from users who disallow adding a link to their account in forwarded messages (string).
+    self.forwardSenderName = data.forward_sender_name
+
+    --- For forwarded messages, date the original message was sent in Unix time (number).
+    self.forwardDate = data.forward_date
+
+    --- For replies, the original message (Message).
+    -- Note that the Message object in this field will not contain further reply_to_message fields even if it itself is a reply.
+    self.replyToMessage = data.reply_to_message and Message(data.reply_to_message)
+
+    --- Date the message was last edited in Unix time (number).
+    self.editDate = data.edit_date
+
+    --- The unique identifier of a media message group this message belongs to (string).
+    self.mediaGroupID = data.media_group_id
+
+    --- Signature of the post author for messages in channels (string).
+    self.authorSignature = data.author_signature
+
+    --- For text messages, the actual UTF-8 text of the message, 0-4096 characters (string).
+    self.text = data.text
+
+    --- For text messages, special entities like usernames, URLs, bot commands, etc. that appear in the text (Array of MessageEntity).
+    -- @field self.entities
+    if data.entities then
+        self.entities = {}
+        for k,v in ipairs(data.entities) do
+            self.entities[k] = MessageEntity(v)
+        end
+    end
+
+    --- For messages with a caption, special entities like usernames, URLs, bot commands, etc. that appear in the caption (Array of MessageEntity).
+    -- @field self.captionEntities
+    if data.caption_entities then
+        self.captionEntities = {}
+        for k,v in ipairs(data.caption_entities) do
+            self.captionEntities[k] = MessageEntity(v)
+        end
+    end
+
+    --- Message is an audio file, information about the file (Audio).
+    self.audio = data.audio and Audio(data.audio)
+
+    --- Message is a general file, information about the file (Document).
+    self.document = data.document and Document(data.document)
+
+    --- Message is an animation, information about the animation. For backward compatibility, when this field is set, the document field will also be set (Animation).
+    self.animation = data.animation and Animation(data.animation)
+
+    --- Message is a game, information about the game (Game).
+    self.game = data.game and Game(data.game)
+
+    --- Message is a photo, available sizes of the photo (Array of PhotoSize).
+    if data.photo then
+        self.photo = {}
+        for k,v in ipairs(data.photo) do
+            self.photo[k] = PhotoSize(v)
+        end
+    end
+
+    --TODO: sticker
+
+    --- Message is a video, information about the video (Video).
+    self.video = data.video and Video(data.video)
+
+    --- Message is a voice message, information about the file (Voice).
+    self.voice = data.voice and Voice(data.voice)
+
+    --- Message is a [video note](https://telegram.org/blog/video-messages-and-telescope), information about the video message (VideoNote).
+    self.videoNote = data.video_note and VideoNote(data.video_note)
+
+    --- Caption for the animation, audio, document, photo, video or voice, 0-1024 characters (string).
+    self.caption = data.caption
+
+    --- Message is a shared contact, information about the contact (Contact).
+    self.contact = data.contact and Contact(data.contact)
+
+    --- Message is a shared location, information about the location (Location).
+    self.location = data.location and Location(data.location)
+
+    --- Message is a venue, information about the venue (Venue).
+    self.venue = data.venue and Venue(data.venue)
+
+    --TODO: poll
+
+    --- Message is a dice with random value from 1 to 6 (Dice).
+    self.dice = data.dice and Dice(data.dice)
+
+    --- New members that were added to the group or supergroup and information about them (the bot itself may be one of these members) (Array of User).
+    if data.new_chat_members then
+        self.newChatMembers = {}
+        for k,v in ipairs(data.new_chat_members) do
+            self.newChatMembers[k] = User(v)
+        end
+    end
+
+    --- A member was removed from the group, information about them (this member may be the bot itself) (User).
+    self.leftChatMember = data.left_chat_member and User(data.left_chat_member)
+
+    --- A chat title was changed to this value (string).
+    self.newChatTitle = data.new_chat_title
+
+    --- A chat photo was change to this value (Array of PhotoSize).
+    if data.new_chat_photo then
+        self.newChatPhoto = {}
+        for k,v in ipairs(data.new_chat_photo) do
+            self.newChatPhoto[k] = PhotoSize(v)
+        end
+    end
+
+    --- Service message: the chat photo was deleted (boolean) (True).
+    self.deleteChatPhoto = data.delete_chat_photo
+
+    --- Service message: the group has been created (boolean) (True).
+    self.groupChatCreated = data.group_chat_created
+
+    --- Service message: the supergroup has been created (boolean) (True).
+    -- This field can‘t be received in a message coming through updates, because bot can’t be a member of a supergroup when it is created.
+    -- It can only be found in replyToMessage if someone replies to a very first message in a directly created supergroup.
+    self.supergroupChatCreated = data.supergroup_chat_created
+
+    --- Service message: the channel has been created (boolean) (True).
+    -- This field can‘t be received in a message coming through updates, because bot can’t be a member of a channel when it is created.
+    -- It can only be found in replyToMessage if someone replies to a very first message in a channel.
+    self.channelChatCreated = data.channel_chat_created
+
+    --- The group has been migrated to a supergroup with the specified identifier (number).
+    self.migrateToChatID = data.migrate_to_chat_id
+
+    --- Specified message was pinned (Message).
+    -- Note that the Message object in this field will not contain further replyToMessage fields even if it is itself a reply.
+    self.pinnedMessage = data.pinned_message and Message(data.pinned_message)
+
+    --TODO: invoice, successfuk_payment
+
+    --- The domain name of the website on which the user has logged in (string).
+    -- [More about Telegram Login](https://core.telegram.org/widgets/login).
+    self.connectedWebsite = data.connected_website
+
+    --TODO: passport_data
+
+    --- Inline keyboard attached to the message (InlineKeyboardMarkup).
+    -- `login_url` buttons are represented as ordinary `url` buttons.
+    self.replyMarkup = data.reply_markup and InlineKeyboardMarkup(data.reply_markup)
+
+    ---
+    -- @section end
+end
 
 return Message
