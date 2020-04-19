36ef7cbf7800094784b6426489fa1b2511def647
Rami Sabbagh <ramilego4game@gmail.com>
2020-04-19 20:02:38 +0300

    Whitespace

--- lua/telegram/structures/User.lua
+++ lua/telegram/structures/User.lua
@@ -13,8 +13,10 @@ function User:initialize(data)
 
     --- Unique identifier for this user or bot (number).
     self.id = data.id
+
     --- True, if this user is a bot (boolean).
     self.isBot = data.is_bot
+
     --- User's or bot's first name (string).
     self.firstName = data.first_name
 
@@ -23,18 +25,23 @@ function User:initialize(data)
 
     --- User‘s or bot’s last name (string).
     self.lastName = data.last_name
+
     --- User‘s or bot’s username (string).
     self.username = data.username
+
     --- [IETF language tag](https://en.wikipedia.org/wiki/IETF_language_tag) of the user's language (string).
     self.languageCode = data.language_code
+
     --- True, if the bot can be invited to groups (boolean).
     -- Returned only in `getMe`.
     -- @see telegram.getMe
     self.canJoinGroups = data.can_join_groups
+
     --- True, if [privacy mode](https://core.telegram.org/bots#privacy-mode) is disabled for the bot (boolean).
     -- Returned only in `getMe`.
     -- @see telegram.getMe
     self.canReadAllGroupMessages = data.can_read_all_group_messages
+
     --- True, if the bot supports inline queries (boolean).
     -- Returned only in `getMe`.
     -- @see telegram.getMe
