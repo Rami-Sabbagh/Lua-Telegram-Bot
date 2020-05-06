d0837fe4fb86a1608d78290a73268948de5522e1
Rami Sabbagh <ramilego4game@gmail.com>
2020-05-06 23:10:46 +0300

    Write the Dice structure

--- lua/telegram/structures/Dice.lua
+++ lua/telegram/structures/Dice.lua
@@ -6,6 +6,27 @@ local class = require("middleclass")
 
 local Dice = class("telegram.structures.Dice")
 
---TODO: Dice object.
+--- Create a new dice object using data returned by Telegram Bot API.
+-- @tparam table data The dice data returned by Telegram Bot API.
+-- @treturn Chat The new created dice object.
+function Dice:initialize(data)
+
+    --- Emoji on which the dice throw animation is based (string).
+    self.emoji = data.emoji
+
+    --- Emoji on which the dice throw animation is based (number).
+    self.value = data.value
+
+end
+
+--- Operators overrides.
+-- @section operators_overrides
+
+--- Test if the 2 dice objects are equal (same emoji, and same value).
+-- @tparam Dice user The dice to compare with.
+-- @treturn boolean `true` if they're the same.
+function Dice:__eq(dice)
+    return (self.emoji == dice.emoji) and (self.value == dice.emoji)
+end
 
 return Dice
