6770c3a32ea1a52c876ed3db936a50ef0d269f12
Rami Sabbagh <ramilego4game@gmail.com>
2020-05-18 15:31:34 +0300

    Implement the `setStickerThumb` method, which means the Stickers functionality is now complete!

--- lua/telegram/init.lua
+++ lua/telegram/init.lua
@@ -377,8 +377,23 @@ function telegram.deleteStickerFromSet(sticker)
     return data
 end
 
---TODO: setStickerThumb
-
-
+--- Use this method to set the thumbnail of a sticker set. Animated thumbnails can be set for animated sticker sets only.
+-- @tparam string name Sticker set name.
+-- @tparam number userID User identifier of the sticker set owner.
+-- @tparam ?InputFile|string|nil A PNG image with the thumbnail, must be up to 128 kilobytes in size and have width and height exactly 100px, or a TGS animation with the thumbnail up to 32 kilobytes in size; see https://core.telegram.org/animated_stickers#technical-requirements for animated sticker technical requirements. Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one using multipart/form-data. Animated sticker set thumbnail can't be uploaded via HTTP URL.
+-- @treturn boolean `true` on success.
+-- @raise Error on failure.
+function telegram.setStickerThumb(name, userID, thumb)
+    local parameters = {name=name, user_id=userID}
+    local ok, data
+    if type(thumb) == "string" then
+        parameters.thumb = thumb
+        ok, data = telegram.request("setStickerThumb", parameters)
+    else
+        ok, data = telegram.request("setStickerThhumb", parameters, nil, {thumb=thumb})
+    end
+    if not ok then return error(data) end
+    return data
+end
 
 return telegram
--- lua/telegram/structures/User.lua
+++ lua/telegram/structures/User.lua
@@ -101,6 +101,15 @@ function User:addStickerToSet(name, pngSticker, tgsSticker, emojis, maskPosition
     return call("addStickerSet", self.id, name, pngSticker, tgsSticker, emojis, maskPosition)
 end
 
+--- Use this method to set the thumbnail of a sticker set. Animated thumbnails can be set for animated sticker sets only.
+-- @tparam string name Sticker set name.
+-- @tparam ?InputFile|string|nil A PNG image with the thumbnail, must be up to 128 kilobytes in size and have width and height exactly 100px, or a TGS animation with the thumbnail up to 32 kilobytes in size; see https://core.telegram.org/animated_stickers#technical-requirements for animated sticker technical requirements. Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one using multipart/form-data. Animated sticker set thumbnail can't be uploaded via HTTP URL.
+-- @treturn boolean `true` on success.
+-- @raise Error on failure.
+function User:setStickerSetThumb(name, thumb)
+    return call("setStickerSetThumb", name, self.id, thumb)
+end
+
 --- Operators overrides.
 -- @section operators_overrides
 
