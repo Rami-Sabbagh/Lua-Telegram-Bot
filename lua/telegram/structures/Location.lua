907fd199cafbd0b3efad710c8c61d8327b3514ee
Rami Sabbagh <ramilego4game@gmail.com>
2020-05-19 14:40:41 +0300

    Implement the `Location` structure.

--- lua/telegram/structures/Location.lua
+++ lua/telegram/structures/Location.lua
@@ -6,6 +6,17 @@ local class = require("middleclass")
 
 local Location = class("telegram.structures.Location")
 
---TODO: Location object.
+--- Create a new location object using data returned by Telegram Bot API.
+-- @tparam table data The location data returned by Telegram Bot API.
+-- @treturn Location The new created location object.
+function Location:initialize(data)
+
+    --- Longitude as defined by sender (number).
+    self.longitude = data.longitude
+
+    --- Latitude as defined by sender (number).
+    self.latitude = data.latitude
+
+end
 
 return Location
