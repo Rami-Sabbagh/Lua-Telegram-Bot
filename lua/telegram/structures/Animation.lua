a77a3a080f34e73a8d1477a0c90d573ff196fc5c
Rami Sabbagh <ramilego4game@gmail.com>
2020-05-19 14:33:23 +0300

    Implement the `Animation` structure.

--- lua/telegram/structures/Animation.lua
+++ lua/telegram/structures/Animation.lua
@@ -4,10 +4,48 @@
 
 local class = require("middleclass")
 
---local PhotoSize = require("telegram.structures.PhotoSize")
+local PhotoSize = require("telegram.structures.PhotoSize")
 
 local Animation = class("telegram.structures.Animation")
 
---TODO: Animation object.
+--- Create a new animation object using data returned by Telegram Bot API.
+-- @tparam table data The animation data returned by Telegram Bot API.
+-- @treturn Animation The new created animation object.
+function Animation:initialize(data)
+
+    --- Identifier for this file, which can be used to download or reuse the file (string).
+    self.fileID = data.fileID
+
+    --- Unique identifier for this file, which is supposed to be the same over time and for different bots. can't be used to download or reuse the file. (string).
+    self.fileUniqueID = data.file_unique_id
+
+    --- Video width as defined by sender (number).
+    self.width = data.width
+
+    --- Video height as defined by sender (number).
+    self.height = data.height
+
+    --- Duration of the video in seconds as defined by sender (number).
+    self.duration = data.duration
+
+    --- Optional fields.
+    -- @section optional_fields
+
+    --- Animation thumbnail as defined by sender (PhotoSize).
+    self.thumb = data.thumb and PhotoSize(data.thumb)
+
+    --- Original animation filename as defined by sender (string).
+    self.fileName = data.file_name
+
+    --- MIME type of the file as defined by sender (string).
+    self.mimeType = data.mime_type
+
+    --- File size (number).
+    self.fileSize = data.file_size
+
+    ---
+    -- @section end
+
+end
 
 return Animation
