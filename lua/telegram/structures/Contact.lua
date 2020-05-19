e59078a694b09e5028c7e8d3e1836ddc0254e8a9
Rami Sabbagh <ramilego4game@gmail.com>
2020-05-19 14:10:34 +0300

    Implement the `Contact` structure.

--- lua/telegram/structures/Contact.lua
+++ lua/telegram/structures/Contact.lua
@@ -6,6 +6,29 @@ local class = require("middleclass")
 
 local Contact = class("telegram.structures.Contact")
 
---TODO: Contact object.
+--- Create a new contact object using data returned by Telegram Bot API.
+-- @tparam table data The contact data returned by Telegram Bot API.
+-- @treturn User The new created contact object.
+function Contact:initialize(data)
+
+    --- Contact's phone number (string).
+    self.phoneNumber = data.phone_number
+
+    --- Contact's first name (string).
+    self.firstName = data.first_name
+
+    --- Optional fields.
+    -- @section optional_fields
+
+    --- Contact's last name (string).
+    self.lastName = data.last_name
+
+    --- Contact's user identifier in Telegram (number).
+    self.userID = data.user_id
+
+    --- Additional data about the contact in the form of a [vCard](https://en.wikipedia.org/wiki/VCard) (string).
+    self.vcard = data.vcard
+
+end
 
 return Contact
