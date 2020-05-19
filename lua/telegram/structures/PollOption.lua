1d09feeb47468ac75c3b31e5f3f289086f459292
Rami Sabbagh <ramilego4game@gmail.com>
2020-05-19 14:38:19 +0300

    Implement the `PollOption` structure.

--- lua/telegram/structures/PollOption.lua
+++ lua/telegram/structures/PollOption.lua
@@ -6,6 +6,17 @@ local class = require("middleclass")
 
 local PollOption = class("telegram.structures.PollOption")
 
---TODO: PollOption object.
+--- Create a new poll option object using data returned by Telegram Bot API.
+-- @tparam table data The poll option data returned by Telegram Bot API.
+-- @treturn PollOption The new created poll option object.
+function PollOption:initialize(data)
+
+    --- Option text, 1-100 characters (string).
+    self.text = data.text
+
+    --- Number of users that voted for this option (number).
+    self.voterCount = data.voter_count
+
+end
 
 return PollOption
