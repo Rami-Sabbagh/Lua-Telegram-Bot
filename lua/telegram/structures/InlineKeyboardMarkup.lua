c0101e0d0988e363ad46027d8401bf2cb0d2c28f
Rami Sabbagh <ramilego4game@gmail.com>
2020-05-23 19:23:20 +0300

    Implement the `InlineKeyboardMarkup` structure.

--- lua/telegram/structures/InlineKeyboardMarkup.lua
+++ lua/telegram/structures/InlineKeyboardMarkup.lua
@@ -6,6 +6,33 @@ local class = require("middleclass")
 
 local InlineKeyboardMarkup = class("telegram.InlineKeyboardMarkup")
 
---TODO: InlineKeyboardMarkup object.
+--- Create a new InlineKeyboardMarkup object.
+-- @tparam {{InlineKeyboardMarkup}} keyboard Array of keyboard rows, each row is an array of InlineKeyboardButton.
+-- @treturn InlineKeyboardMarkup The new created InlineKeyboardMarkup object.
+-- @usage local InlineKeyboardMarkup = telegram.structures.InlineKeyboardMarkup(keyboard)
+function InlineKeyboardMarkup:initialize(keyboard)
+
+    --- Array of button rows, each represented by an Array of `InlineKeyboardButton` objects.
+    self.inlineKeyboard = keyboard
+
+end
+
+--- Get the object's data in telegram's API format, to be sent using a method.
+-- @treturn table The object's data in telegram's API format.
+function InlineKeyboardMarkup:getData()
+    local keyboard = {}
+
+    for rid, row in pairs(self.keyboard) do
+        local clonedRow = {}
+        for bid, button in pairs(row) do
+            clonedRow[bid] = button:getData()
+        end
+        keyboard[rid] = clonedRow
+    end
+
+    return {
+        inline_keyboard = keyboard
+    }
+end
 
 return InlineKeyboardMarkup
